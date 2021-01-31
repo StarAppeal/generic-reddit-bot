73770e2627984761c48d9d5216ea8d6884ba5669
StarAppeal <info@starappeal.app>
2021-01-31 19:08:24 +0100

    fix style

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,23 +1,19 @@
-const {
-  createLogger,
-  format,
-  transports
-} = require('winston');
+const { createLogger, format, transports } = require("winston");
 
 const logger = createLogger({
-  level: 'info',
+  level: "info",
   format: format.combine(
     format.timestamp({
-      format: 'DD.MM.YYYY HH:mm:ss'
+      format: "DD.MM.YYYY HH:mm:ss",
     }),
     format.errors({
-      stack: true
+      stack: true,
     }),
     format.splat(),
     format.json()
   ),
   defaultMeta: {
-    service: process.env.BOT_NAME
+    service: process.env.BOT_NAME,
   },
   transports: [
     //
@@ -25,30 +21,29 @@ const logger = createLogger({
     // - Write all logs error (and below) to `quick-start-error.log`.
     //
     new transports.File({
-      filename: 'error.log',
-      level: 'error'
+      filename: "error.log",
+      level: "error",
     }),
     new transports.File({
-      filename: 'info.log',
-      level: 'info'
+      filename: "info.log",
+      level: "info",
     }),
     new transports.File({
-      filename: 'combined.log'
-    })
-  ]
+      filename: "combined.log",
+    }),
+  ],
 });
 
 //
 // If we're not in production then **ALSO** log to the `console`
 // with the colorized simple format.
 //
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.combine(
-      format.colorize(),
-      format.simple()
-    )
-  }));
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    })
+  );
 }
 
 module.exports = logger;
