28eceb70119ecc4c5487851695562adc1d6cf05b
StarAppeal <silkfan11@gmail.com>
2021-01-31 19:08:24 +0100

    fix style

--- src/config/streamConfig.js
+++ src/config/streamConfig.js
@@ -1,21 +1,20 @@
-const {
-  SubmissionStream
-} = require("snoostorm");
+const { SubmissionStream } = require("snoostorm");
 
-const Snoowrap = require('snoowrap');
-const Snoostorm = require('snoostorm');
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
 
 const r = new Snoowrap({
-  userAgent: 'linux:'+process.env.BOT_NAME+':1.0 (by ' + process.env.DEVELOPER + ')',
+  userAgent:
+    "linux:" + process.env.BOT_NAME + ":1.0 (by " + process.env.DEVELOPER + ")",
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  refreshToken: process.env.REFRESH_TOKEN
+  refreshToken: process.env.REFRESH_TOKEN,
 });
 
 const submissionstream = new SubmissionStream(r, {
   subreddit: process.env.SUBREDDIT,
   limit: parseInt(process.env.POLL_LIMIT),
-  pollTime: parseInt(process.env.POLL_TIME)
+  pollTime: parseInt(process.env.POLL_TIME),
 });
 
-module.exports = {stream: submissionstream, wrap: r};
+module.exports = { stream: submissionstream, wrap: r };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const app = require('./app');
-const http = require('http');
+const app = require("./app");
+const http = require("http");
 const roboter = require("./roboter/genericBoter");
 
-var port = process.argv[2] === 'DEBUG' ? 81 : process.env.PORT;
+var port = process.argv[2] === "DEBUG" ? 81 : process.env.PORT;
 
-http.createServer(app).listen(port, function() {
+http.createServer(app).listen(port, function () {
   console.log("App listens on port: " + this.address().port);
   roboter.startStream();
 });
