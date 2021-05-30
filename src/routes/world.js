90e8e74d1a18b4025fc3cf924305223e4dff2158
StarAppeal <silkfan11@gmail.com>
2021-05-30 22:37:05 +0200

    reverse test commits

--- src/routes/world.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/world", function (req, res, next) {
-  res.status(200).send("Hello World!");
-});
-
-module.exports = router;
