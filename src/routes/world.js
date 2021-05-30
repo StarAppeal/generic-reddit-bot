ca008fe2b83996bd382ab08c84103ecdc36aa096
StarAppeal <silkfan11@gmail.com>
2021-05-30 22:34:21 +0200

    change it to be english

--- src/routes/world.js
+++ src/routes/world.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/welt", function (req, res, next) {
-  res.status(200).send("Hallo Welt!");
+router.get("/world", function (req, res, next) {
+  res.status(200).send("Hello World!");
 });
 
 module.exports = router;
