a5492010ae073158cd7a2549d63c85f2f32d5a30
StarAppeal <silkfan11@gmail.com>
2022-03-04 23:49:26 +0100

    change debug variable to node_env

--- src/creator/loggerCreator.js
+++ src/creator/loggerCreator.js
@@ -35,7 +35,7 @@ function _createLogger(botConfig){
   // If we're not in production then **ALSO** log to the `console`
   // with the colorized simple format.
   //
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV === "development") {
     logger.add(
       new transports.Console({
         format: format.combine(format.colorize(), format.simple()),
--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+// set development if node_env => undefined
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 const GenericBot = require("./bot/genericBot")
 
 const bots = require("./config/robots.json").bots;
