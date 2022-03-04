5062692e44098c0c562c1dac0e502b2e4f5f9ab6
StarAppeal <info@starappeal.app>
2022-03-04 23:49:26 +0100

    change debug variable to node_env

--- src/bot/handlers/commentHandler.js
+++ src/bot/handlers/commentHandler.js
@@ -1,10 +1,13 @@
+const debug = process.env.NODE_ENV === "development";
+
 module.exports = class CommentHandler {
     constructor(comment, logger) {
         this.comment = comment;
         this.logger = logger;
     }
 
-    async reply(text, debug = false) {   
+    async reply(text) {   
+        this.logger.info(debug);
         if (debug) {
             this.logger.info("Not replying because you are developing");
             return false;
